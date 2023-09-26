fe89b261194556caf9296ac368fa182bcd648e2c
PRATIK NOUBAGH <pratik.noubagh23@gmail.com>
2023-09-26 07:05:58 +0000

    Employee app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Create a list to store employee names
+
 employee_names = []
 
 
@@ -13,14 +13,13 @@ def index():
 
 @app.route('/add_employee', methods=['POST'])
 def add_employee():
-    # Get the employee name from the form
+   
     employee_name = request.form.get('employee_name')
 
-    # Add the employee name to the list
     if employee_name:
         employee_names.append(employee_name)
 
-    # Redirect back to the index page
+    
     return redirect(url_for('index'))
 
 
